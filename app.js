3938a5991ecfff3b41fc6e401e0cd74f34c6211e
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-13 08:29:39 -0300

    FlorestaOng

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
-const url = 'http://localhost:3000/produtos'
-fetch(url)
- .then((req) => req.json())
- .then((data) => mostraProdutos(data));
-
-function mostraProdutos(produtos){
- const htmlProdutos = produtos.map(
- (produto) =>`
- <img src=${produto.imagem} width=100 height=100><br>
- <h2>${produto.descricao}</h2>
- <h4>Valor : ${produto.valor}</h4>`
- );
- document.getElementById('app').innerHTML = htmlProdutos;
-}
+const firebaseConfig = {
+    apiKey: "AIzaSyDmqtIz8Y0Uws1uNRvB1aV4Ni6FXjmkyI4",
+    authDomain: "at--ong.firebaseapp.com",
+    projectId: "at--ong",
+    storageBucket: "at--ong.appspot.com",
+    messagingSenderId: "27022439477",
+    appId: "1:27022439477:web:999ad88bd875dd3ea3ed7c"
+  };
+  
+firebase.initializeApp(firebaseConfig);
+ 
+const emailField = document.getElementById('email');
+const passwordField = document.getElementById('password');
+const loginButton = document.getElementById('loginButton');
+loginButton.addEventListener('click', () => {
+ const email = emailField.value;
+ const password = passwordField.value;
+ firebase.auth().signInWithEmailAndPassword(email, password)
+ .then((userCredential) => {
+ // Usuário logado com sucesso
+ const user = userCredential.user;
+ console.log('Usuário logado:', user);
+ })
+ .catch((error) => {
+ // Tratar erros de autenticação
+ const errorMessage = error.message;
+ console.error('Erro de autenticação:', errorMessage);
+ });
+});
